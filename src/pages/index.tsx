57fda021209453481088665e7dde628abf14de56
Futhememe <kyoro_souza@hotmail.com>
2023-01-26 16:05:49 -0300

    🎨 add banner e introduction component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import { Banner } from "@/components/Banner";
+import { Connecting } from "@/components/Connecting";
 import { Header } from "@/components/Header";
 import { Heading } from "@/components/Heading";
-import { HomeContainer } from "@/components/Home";
+import { HomeContainer, IntroductionSection } from "@/components/Home";
 import { LinkButton } from "@/components/LinkButton";
 import { Main } from "@/components/Main";
 import Head from "next/head";
@@ -24,6 +26,13 @@ export default function Home() {
           </div>
           <LinkButton>My Works</LinkButton>
         </HomeContainer>
+        <Banner />
+        <IntroductionSection>
+          <Heading size="3xl">
+            Taking your business to the next level connecting code and design
+          </Heading>
+          <Connecting />
+        </IntroductionSection>
       </Main>
     </>
   );
