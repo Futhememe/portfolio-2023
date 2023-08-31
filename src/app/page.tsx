df23b8279c48c5054ff121376b6fdaa3521e8ccc
Futheme <kyoro_souza@hotmail.com>
2023-08-30 23:20:45 -0300

    🎨 update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
 import { css } from "@/styled-system/css";
-import { Hero } from "@/components/Sessions/Hero";
-import { About } from "@/components/Sessions/About";
 
 export default function Home() {
   return (
     <main
       className={css({
         display: "flex",
+        flexDir: "column",
         minH: "100vh",
         maxH: "100vh",
-        minW: "100vw",
+        minW: "calc(100vw - 1rem)",
+        maxW: "calc(100vw - 1rem)",
         overflowY: "hidden",
 
-        bgColor: "yellowspecial",
+        bgColor: "background",
       })}
     >
       <Header />
       <Hero />
-      <About />
     </main>
   );
 }
