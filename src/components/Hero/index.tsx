e166b398632b1206d927e7486bae0fe0f96bf66f
Futheme <kyoro_souza@hotmail.com>
2023-09-02 15:59:07 -0300

    💫 add text animation for hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,19 @@
 import { css } from "@/styled-system/css";
+import { motion, useInView } from "framer-motion";
 import { Display, Text } from "../Typograph";
+import { useRef } from "react";
+import { SlideInState, slideIn } from "@/utils/animations/slideIn";
 
 export const Hero = () => {
+  const firstLineRef = useRef(null);
+  const isFirstLineInView = useInView(firstLineRef, { once: true });
+
+  const secondLineRef = useRef(null);
+  const isSecondLineInView = useInView(secondLineRef, { once: true });
+
+  const thirdLineRef = useRef(null);
+  const isThirdLineInView = useInView(thirdLineRef, { once: true });
+
   return (
     <div
       className={css({
@@ -11,27 +23,65 @@ export const Hero = () => {
         alignItems: "center",
         justifyContent: "center",
 
-        maxH: "calc(100vh - 3.4625rem)",
-        minH: "calc(100vh - 3.4625rem)",
+        maxH: "calc(100vh - 2rem)",
+        minH: "calc(100vh - 2rem)",
         position: "relative",
       })}
     >
-      <Display css={{ fontSize: "4rem", lineHeight: "tight" }}>
-        Hello, {"I'm"} <b className={css({ fontWeight: 500 })}>Gustavo</b>
-      </Display>
-      <Display css={{ fontSize: "4rem", lineHeight: "tight" }}>
-        an creative designer
-      </Display>
-      <Display css={{ fontSize: "4rem", lineHeight: "tight" }}>
-        and developer
-      </Display>
+      <div
+        className={css({
+          display: "flex",
+          flexDir: "column",
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          mt: "-6rem",
+        })}
+      >
+        <motion.div
+          ref={firstLineRef}
+          animate={
+            isFirstLineInView ? SlideInState.VISIBLE : SlideInState.OUTOFVIEW
+          }
+          variants={slideIn}
+          transition={{ delay: 0.2 }}
+        >
+          <Display css={{ fontSize: "4rem", lineHeight: "tight" }}>
+            Hello, {"I'm"} <b className={css({ fontWeight: 500 })}>Gustavo</b>
+          </Display>
+        </motion.div>
+        <motion.div
+          ref={secondLineRef}
+          animate={
+            isSecondLineInView ? SlideInState.VISIBLE : SlideInState.OUTOFVIEW
+          }
+          variants={slideIn}
+          transition={{ delay: 0.6 }}
+        >
+          <Display css={{ fontSize: "4rem", lineHeight: "tight" }}>
+            an creative designer
+          </Display>
+        </motion.div>
+        <motion.div
+          ref={thirdLineRef}
+          animate={
+            isThirdLineInView ? SlideInState.VISIBLE : SlideInState.OUTOFVIEW
+          }
+          variants={slideIn}
+          transition={{ delay: 1 }}
+        >
+          <Display css={{ fontSize: "4rem", lineHeight: "tight" }}>
+            and developer
+          </Display>
+        </motion.div>
+      </div>
 
       <Text
         css={{
           fontSize: "1rem",
           fontWeight: 501,
           position: "absolute",
-          bottom: "6.5rem",
+          bottom: "7.5rem",
         }}
       >
         scroll for more
