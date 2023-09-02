0ee89c4823b6e01954083848ec0bba70ee93bf70
Futheme <kyoro_souza@hotmail.com>
2023-09-02 13:28:33 -0300

    🚧 creating the skill set tags

---
+++ src/components/SkillSet/index.tsx
@@ -0,0 +1,42 @@
+import { css } from "@/styled-system/css";
+import { Display } from "../Typograph";
+import { SkillTag } from "./styles";
+
+interface ISkillSet {
+  title: string;
+  tags: string[];
+}
+
+export const SkillSet = ({ title, tags }: ISkillSet) => {
+  return (
+    <div
+      className={css({
+        display: "flex",
+        flexDir: "column",
+        alignItems: "center",
+        textWrap: "wrap",
+        textAlign: "center",
+
+        maxW: "34.3125rem",
+        gap: "1rem",
+      })}
+    >
+      <Display css={{ fontSize: "6rem", lineHeight: "6rem" }}>{title}</Display>
+      <div
+        className={css({
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "1rem",
+
+          alignItems: "center",
+          justifyContent: "center",
+          maxW: "25.5rem",
+        })}
+      >
+        {tags.map((tag) => (
+          <SkillTag key={tag}>{tag}</SkillTag>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/SkillSet/styles.tsx
@@ -0,0 +1,19 @@
+import { styled } from "@/styled-system/jsx";
+
+export const SkillTag = styled("div", {
+  base: {
+    display: "flex",
+    padding: "0.5rem",
+    justifyContent: "center",
+    alignItems: "center",
+
+    borderRadius: "0.5rem",
+    bgColor: "darkbackground",
+
+    fontFamily: "body",
+    fontSize: "1rem",
+    fontWeight: 501,
+
+    color: "background",
+  },
+});
