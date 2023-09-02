a3001a5197ebb777d2573ca30c0d4a957285bfc7
Futheme <kyoro_souza@hotmail.com>
2023-09-02 13:32:13 -0300

    🚧 creating work together session

---
+++ src/components/WorkTogether/index.tsx
@@ -0,0 +1,14 @@
+import { Display } from "../Typograph";
+
+export const WorkTogether = () => {
+  return (
+    <div>
+      <div>
+        <Display>Let{"'"}s</Display>
+        <div />
+        <Display>work</Display>
+      </div>
+      <Display>together</Display>
+    </div>
+  );
+};
