d9355db0c8f5d455d3b535c7929c4c9a26044727
Futheme <kyoro_souza@hotmail.com>
2023-09-09 18:54:18 -0300

    ✨ new experience line

---
+++ src/components/About/ExperienceLine/index.tsx
@@ -0,0 +1,49 @@
+import { SkillTag } from "@/components/SkillSet/styles";
+import { Text } from "@/components/Typograph";
+import { css } from "@/styled-system/css";
+import { styled } from "@/styled-system/jsx";
+
+interface IExperienceLine {
+  company: string;
+  roles: string[];
+  timeline: string;
+}
+
+export const ExperienceLine = ({
+  company,
+  roles,
+  timeline,
+}: IExperienceLine) => {
+  return (
+    <div
+      className={css({
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        w: "100%",
+      })}
+    >
+      <div className={css({ display: "flex", flexDir: "column" })}>
+        <Text css={{ fontSize: "2rem", fontWeight: 500 }}>{company}</Text>
+        <div
+          className={css({
+            display: "flex",
+            alignItems: "center",
+            gap: "0.625rem",
+          })}
+        >
+          {roles?.map((role) => (
+            <Tag key={role}>{role}</Tag>
+          ))}
+        </div>
+      </div>
+      <Text>{timeline}</Text>
+    </div>
+  );
+};
+
+export const Tag = styled(SkillTag, {
+  base: {
+    fontSize: "0.875rem",
+  },
+});
