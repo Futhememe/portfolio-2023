3d437abdd2a5aed0fd1537ba26d902cd3f14f9c7
Futheme <kyoro_souza@hotmail.com>
2023-09-16 17:44:20 -0300

    🎨 create compond pattern for form components

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,11 @@
+import { Input } from "./Input";
+import { Root } from "./Root";
+import { Submit } from "./Submit";
+import { Textarea } from "./Textarea";
+
+export const Form = {
+  Root,
+  Input,
+  Textarea,
+  Submit,
+};
