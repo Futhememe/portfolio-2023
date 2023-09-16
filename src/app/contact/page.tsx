c7fadd7d675d2864b19d2652aba845581636b479
Futheme <kyoro_souza@hotmail.com>
2023-09-16 17:42:43 -0300

    🚧 new page contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { Form } from "@/components/Form";
+import { Display } from "@/components/Typograph";
+import { useTrailerMouse } from "@/hooks/useTrailerMouse";
+import { css } from "@/styled-system/css";
+
+export default function Contact() {
+  const { contactEnter, mouseLeave } = useTrailerMouse();
+
+  return (
+    <div
+      className={css({
+        display: "flex",
+        flexDir: "column",
+        alignItems: "center",
+        w: "100%",
+        p: "4rem",
+        minH: "100vh",
+        position: "relative",
+      })}
+    >
+      <Display css={{ fontSize: "6rem" }}>Contact</Display>
+      <Form.Root>
+        <Form.Input
+          name="name"
+          label="What's your name ?"
+          placeholder="Your name"
+        />
+
+        <Form.Input
+          name="email"
+          type="email"
+          label="What's your email ?"
+          placeholder="Your email"
+        />
+
+        <Form.Input
+          name="phone"
+          label="What's your phone number ?"
+          placeholder="Your phone number"
+        />
+
+        <Form.Textarea
+          name="about"
+          label="Tell me more about your project*"
+          placeholder="Describe it as you can"
+        />
+
+        <Form.Submit
+          onMouseEnter={contactEnter}
+          onMouseLeave={mouseLeave}
+          type="button"
+          style={{ marginTop: "2rem" }}
+        >
+          Send
+        </Form.Submit>
+      </Form.Root>
+    </div>
+  );
+}
