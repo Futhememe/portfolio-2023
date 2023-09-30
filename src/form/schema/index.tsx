a1e620c9ef270f0d54171a69c730f6488bafa7a1
Futheme <kyoro_souza@hotmail.com>
2023-09-30 16:32:21 -0300

    🎨 add min value validation

--- src/form/schema/index.tsx
+++ src/form/schema/index.tsx
@@ -1,10 +1,11 @@
 import { z } from "zod";
 
 export type IContactSchema = z.input<typeof contactSchema>;
+export type IContactSchemaOutput = z.output<typeof contactSchema>;
 
 export const contactSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
+  name: z.string().min(1, { message: "required" }),
+  email: z.string().email().min(1, { message: "required" }),
   phone: z.string().optional(),
-  description: z.string(),
+  description: z.string().min(1, { message: "required" }),
 });
