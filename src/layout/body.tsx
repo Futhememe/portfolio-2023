5dce9b10d415d13352f68e24427bef98f543d9ce
Futheme <kyoro_souza@hotmail.com>
2023-12-23 21:28:19 -0300

    💫 add animation to all pages

--- src/layout/body.tsx
+++ src/layout/body.tsx
@@ -1,6 +1,8 @@
 import { Footer } from "@/components/Footer";
 import { TrailerMouseProvider } from "@/hooks/useTrailerMouse";
 import { css } from "@/styled-system/css";
+import Inner from "@/utils/animations/Inner";
+import Stairs from "@/utils/animations/Stairs";
 import { ReactNode } from "react";
 
 interface IBody {
@@ -9,25 +11,25 @@ interface IBody {
 
 export const Body = ({ children }: IBody) => {
   return (
-    <div
-      className={css({
-        display: "flex",
-        flexDir: "column",
-        alignItems: "center",
-        minW: ["100vw", "calc(100vw - 1rem)"],
-        maxW: ["100vw", "calc(100vw - 1rem)"],
-        minH: "100vh",
-        margin: 0,
-        p: 0,
+    <Stairs>
+      <div
+        className={css({
+          display: "flex",
+          flexDir: "column",
+          alignItems: "center",
+          minW: ["100vw", "calc(100vw - 1rem)"],
+          maxW: ["100vw", "calc(100vw - 1rem)"],
+          minH: "100vh",
+          margin: 0,
+          p: 0,
 
-        bgColor: "background",
-        position: "relative",
-      })}
-    >
-      <TrailerMouseProvider>
+          bgColor: "background",
+          position: "relative",
+        })}
+      >
         {children}
         <Footer />
-      </TrailerMouseProvider>
-    </div>
+      </div>
+    </Stairs>
   );
 };
