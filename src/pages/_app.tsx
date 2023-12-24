947c449eaa416f6aae0df3f75c64d68253a6f8ef
Futheme <kyoro_souza@hotmail.com>
2023-12-23 21:29:27 -0300

    🎨 pass trailer mouse to root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import { Footer } from "@/components/Footer";
 import { TrailerMouseProvider } from "@/hooks/useTrailerMouse";
-import { Body } from "@/layout/body";
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <AnimatePresence mode="wait">
-      <Component key={router.route} {...pageProps} />
-    </AnimatePresence>
+    <TrailerMouseProvider>
+      <AnimatePresence mode="wait">
+        <Component key={router.route} {...pageProps} />
+      </AnimatePresence>
+    </TrailerMouseProvider>
   );
 }
