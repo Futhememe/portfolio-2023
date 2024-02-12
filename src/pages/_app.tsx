7a9e9b200f02d5fa39bab23f36441c21c0587d94
Futheme <kyoro_souza@hotmail.com>
2024-02-12 18:41:09 -0300

    ✨ add provider for react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,19 @@ import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <TrailerMouseProvider>
-      <AnimatePresence mode="wait">
-        <Component key={router.route} {...pageProps} />
-      </AnimatePresence>
-      <SpeedInsights />
-    </TrailerMouseProvider>
+    <QueryClientProvider client={queryClient}>
+      <TrailerMouseProvider>
+        <AnimatePresence mode="wait">
+          <Component key={router.route} {...pageProps} />
+        </AnimatePresence>
+        <SpeedInsights />
+      </TrailerMouseProvider>
+    </QueryClientProvider>
   );
 }
